fbdcb7fc6be0d874f17bfa8e9240cf625635d836
Jordan Van Aalsburg <jordan.vanaalsburg@usace.army.mil>
2024-10-24 16:29:42 -0700

    Create database schema and add user records

---
+++ data/000-init-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL
+);
---
+++ data/001-add-users.sql
@@ -0,0 +1,21 @@
+INSERT INTO
+    users (id, first_name, last_name, email)
+VALUES
+    (
+        '00000000-0000-0000-0000-000000000001',
+        'Harry',
+        'Potter',
+        'hpotter@hogwarts.edu'
+    ),
+    (
+        '00000000-0000-0000-0000-000000000002',
+        'Ron',
+        'Weasley',
+        'rweasley@hogwarts.edu'
+    ),
+    (
+        '00000000-0000-0000-0000-000000000003',
+        'Hermione',
+        'Granger',
+        'hgranger@hogwarts.edu'
+    );
